43a25ebec1eda9a5bb1850349de2e234a7cbf63a
Artur <74742488+Turchikz@users.noreply.github.com>
2026-09-23 14:44:07 +0500

    Add API permissions and permission tests (#12)

--- metrology/tests/conftest.py
+++ metrology/tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from metrology.models import Instrument
 
+from rest_framework.test import APIClient
+
 
 @pytest.fixture
 def instrument():
@@ -13,3 +15,27 @@ def instrument():
         functional_unit=Instrument.FunctionalUnit.BIK,
         status=Instrument.Status.WORK,
     )
+
+
+@pytest.fixture
+def authenticated_client(django_user_model):
+    user = django_user_model.objects.create_user(
+        username="test user",
+    )
+    client = APIClient()
+    client.force_authenticate(user=user)
+
+    return client
+
+
+@pytest.fixture
+def admin_client(django_user_model):
+    admin = django_user_model.objects.create_user(
+        username="admin_user",
+        is_staff=True,
+    )
+
+    client = APIClient()
+    client.force_authenticate(user=admin)
+
+    return client
--- metrology/views.py
+++ metrology/views.py
@@ -1,14 +1,25 @@
 from rest_framework import status, viewsets
+from rest_framework.authentication import (
+    BasicAuthentication,
+    SessionAuthentication,
+)
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models.deletion import ProtectedError
 
 
 from .models import Instrument, Verification
+from .permissions import MetrologyPermission
 from .serializers import InstrumentSerializer, VerificationSerializer
 
 
 class InstrumentViewSet(viewsets.ModelViewSet):
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+    ]
+    permission_classes = [MetrologyPermission]
     queryset = Instrument.objects.all()
     serializer_class = InstrumentSerializer
     filter_backends = [DjangoFilterBackend]
@@ -33,6 +44,11 @@ class InstrumentViewSet(viewsets.ModelViewSet):
 
 
 class VerificationViewSet(viewsets.ModelViewSet):
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+    ]
+    permission_classes = [MetrologyPermission]
     queryset = Verification.objects.all()
     serializer_class = VerificationSerializer
     filter_backends = [DjangoFilterBackend]
